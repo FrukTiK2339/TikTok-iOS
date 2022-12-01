301f4ce167b676f93bef7d49ebf4df3a8edc3731
Dmitry Rybakov <d.rybakov@devplex.ru>
2022-12-01 12:44:53 +0300

    Part 5 ended

--- TikTok/Controllers/Other/CaptionViewController.swift
+++ TikTok/Controllers/Other/CaptionViewController.swift
@@ -5,12 +5,25 @@
 //  Created by Дмитрий Рыбаков on 29.11.2022.
 //
 
-import Foundation
+import ProgressHUD
 import UIKit
 
 class CaptionViewController: UIViewController {
     
     let videoURL: URL
+    
+    private let captionTextView: UITextView = {
+        let textView = UITextView()
+        textView.contentInset = UIEdgeInsets(top: 3, left: 3, bottom: 3, right: 3)
+        textView.backgroundColor = .secondarySystemBackground
+        textView.layer.cornerRadius = 8
+        textView.layer.masksToBounds = true
+        
+        return textView
+    }()
+    
+    
+    // MARK: - Init
     
     init(videoURL: URL) {
         self.videoURL = videoURL
@@ -26,27 +39,44 @@ class CaptionViewController: UIViewController {
         title = "Add Caption"
         view.backgroundColor = .systemBackground
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post", style: .done, target: self, action: #selector(didTapPost))
+        view.addSubview(captionTextView)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        captionTextView.frame = CGRect(x: 5, y: view.safeAreaInsets.top+5, width: view.width-10, height: 150).integral
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captionTextView.becomeFirstResponder()
     }
     
     @objc private func didTapPost() {
+        captionTextView.resignFirstResponder()
+        let caption = captionTextView.text ?? ""
+        
         //Generate video
         let newVideoName = StorageManager.shared.generateVideoName()
+        
+        ProgressHUD.show("Posting")
         
         //Upload video
         StorageManager.shared.uploadVideo(from: videoURL, filename: newVideoName) { [weak self] success in
             if success {
                 DispatchQueue.main.async {
                     //Update database
-                    DatabaseManager.shared.insertPost(fileName: newVideoName) { databaseUpdated in
+                    DatabaseManager.shared.insertPost(fileName: newVideoName, caption: caption) { databaseUpdated in
                         if databaseUpdated {
+                            HapticsManager.shared.vibrate(for: .success)
+                            ProgressHUD.dismiss()
                             self?.navigationController?.popToRootViewController(animated: true)
                             self?.tabBarController?.selectedIndex = 0
                             self?.tabBarController?.tabBar.isHidden = false
                         } else {
+                            HapticsManager.shared.vibrate(for: .error)
+                            ProgressHUD.dismiss()
                             let alert = UIAlertController(title: "Woops", message: "Could not upload video. Try again", preferredStyle: .alert)
                             alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
                             self?.present(alert, animated: true)
@@ -57,6 +87,8 @@ class CaptionViewController: UIViewController {
                 }
                 
             } else {
+                HapticsManager.shared.vibrate(for: .error)
+                ProgressHUD.dismiss()
                 let alert = UIAlertController(title: "Woops", message: "Could not upload video. Try again", preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "Dismiss", style: .cancel))
                 self?.present(alert, animated: true)
--- TikTok/Managers/DatabaseManager.swift
+++ TikTok/Managers/DatabaseManager.swift
@@ -69,7 +69,7 @@ final class DatabaseManager {
         }
     }
     
-    public func insertPost(fileName: String, completion: @escaping (Bool) -> Void) {
+    public func insertPost(fileName: String, caption: String, completion: @escaping (Bool) -> Void) {
         guard let username = UserDefaults.standard.string(forKey: "username") else {
             return
         }
@@ -79,8 +79,14 @@ final class DatabaseManager {
                 completion(false)
                 return
             }
-            if var posts = value["posts"] as? [String] {
-                posts.append(fileName)
+
+            let newEntry = [
+                "name": fileName,
+                "caption": caption
+            ]
+            
+            if var posts = value["posts"] as? [[String: Any]] {
+                posts.append(newEntry)
                 value["posts"] = posts
                 self?.database.child("users").child(username).setValue(value) { error, _ in
                     guard error == nil else {
@@ -90,7 +96,7 @@ final class DatabaseManager {
                     completion(true)
                 }
             } else {
-                value["posts"] = [fileName]
+                value["posts"] = [newEntry]
                 self?.database.child("users").child(username).setValue(value) { error, _ in
                     guard error == nil else {
                         completion(false)
